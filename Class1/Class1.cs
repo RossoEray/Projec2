0a0dd7ac377f5ae1fe4d4c655f8c6c5e878b1f53
Eray Kirazlı <erenkirazli06@gmail.com>
2024-06-10 21:29:49 +0300

    Add project files.

---
+++ Class1/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Ev.Libs
+{
+
+    public class Ev
+    {
+        public byte oda_sayisi { get; set; }
+        public byte kat_sayisi { get; set; }
+        public string semt { get; set; }
+        public byte tuvalet_sayisi { get; set; }
+        public float depozito { get; set; }
+        public virtual string EvBilgileriGetir()
+        {
+            return $"Evin Oda Sayısı:{oda_sayisi}\nEvin Kat Sayısı:{kat_sayisi}\nEvin Semti:{semt}\n" +
+                $"Tuvalet Sayısı:{tuvalet_sayisi}\nDepozito:{depozito}";
+        }
+    }
+    public class SatilikEv : Ev
+    {
+        public int ev_fiyati { get; set; }
+        public override string EvBilgileriGetir()
+        {
+            return $"{base.EvBilgileriGetir()}\nEvin Fiyatı:{ev_fiyati}\n*******";
+        }
+
+    }
+    public class KiralikEv : Ev
+    {
+        public int kira { get; set; }
+        public override string EvBilgileriGetir()
+        {
+            return $"{base.EvBilgileriGetir()}\nEvin Kirası:{kira}\n*******";
+        }
+
+
+    }
+}
